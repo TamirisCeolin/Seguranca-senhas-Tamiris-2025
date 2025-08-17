5183e7c2c0539981360b1217ee9fffe6a7cd7308
Tamiris Ceolin <125214749+TamirisCeolin@users.noreply.github.com>
2025-08-16 21:11:07 -0300

    16/08

--- main.js
+++ main.js
@@ -42,19 +42,16 @@ function geraSenha() {
     if (checkbox[0].checked) {
         alfabeto = alfabeto + letrasMaiusculas;
     }
-
     if (checkbox[1].checked) {
         alfabeto = alfabeto + letrasMinusculas;
     }
-
     if (checkbox[2].checked) {
         alfabeto = alfabeto + numeros;
     }
-
     if (checkbox[3].checked) {
         alfabeto = alfabeto + simbolos;
     }
-
+    console.log(alfabeto);
     let senha = '';
     for (let i = 0; i < tamanhoSenha; i++) {
         let numeroAleatorio = Math.random() * alfabeto.length;
@@ -63,4 +60,3 @@ function geraSenha() {
     }
     campoSenha.value = senha;
 }
-
